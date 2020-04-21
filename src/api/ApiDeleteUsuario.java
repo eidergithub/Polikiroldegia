aeb81543096d6775bea9fd1aacfb1d10ca315352
Eider <Eider@DESKTOP-8BK6AMO>
2020-04-21 17:18:16 +0200

    ApiDeleteUsuario egin

---
+++ src/api/ApiDeleteUsuario.java
@@ -0,0 +1,53 @@
+package api;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class ApiDeleteUsuario
+ */
+@WebServlet("/ApiDeleteUsuario")
+public class ApiDeleteUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiDeleteUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		try {
+			ModeloUsuario mUsuario = new ModeloUsuario();
+			mUsuario.delete(id);
+			mUsuario.getConexion().close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		response.setHeader("Access-Control-Allow-Origin", "*");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
