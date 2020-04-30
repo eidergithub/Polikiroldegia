0d4e409b60234191a9fc8a2b9fb5e660768deaac
Eider <Eider@DESKTOP-8BK6AMO>
2020-04-30 12:11:03 +0200

    DniComparator klasea eginda

---
+++ src/modelo/util/DniComparator.java
@@ -0,0 +1,13 @@
+package modelo.util;
+
+import java.util.Comparator;
+
+import modelo.bean.Usuario;
+
+public class DniComparator implements Comparator<Usuario>{
+	
+	public int compare(Usuario o1, Usuario o2) {
+		return o1.getDni().compareToIgnoreCase(o2.getDni());
+		// return o1.getEdad() - o2.getEdad();
+	}
+}
